de39b17b4d2f1f7fb12ae3759d0b8f7c933adc81
Jeff Biggus <hyperjeff@mac.com>
2021-01-14 19:01:50 -0600

    Sandbox-sanctioned, app-only solution

--- README.md
+++ README.md
@@ -8,17 +8,18 @@ Probably could just make a system service or Safari plugin to do this, but this 
 ## Installation
 There are binaries to download, or you can build and run via Xcode. Works back to macOS 10.14 (Catalina).
 
-_BUT_, because of sandboxing, currently there is a hack to make this all work. There is a `tabometer.sh` script that needs to be installed somewhere (`/usr/local/bin/` or `~/bin` etc) and make it executable (`chmod +x tabometer.sh`) and then run it in the background via Terminal (`tabometer &`). This copies the key file from Safari so that it can be inspected by the Tabometer menu bar app.
+The only small inconvenience about running this app is that the user _must_ select their Safari `LastSession.plist` file for it to be used by this app. The app makes this as easy as I know how to do: An open file dialog is presented to the user on first launch, and it auto-selects the file it needs, and just asks the user to press the `[Select]` button. Once this is done, the user never has to do it again on that machine. (If you pick the wrong file because you don't like being told what or how to do things, then you'll just get a new dialog asking for the same thing until you comply.)
+
+## Building
+If you want to build from source, just make sure to put in your own bundle identifier and developer team and it should build just fine.
 
 ## Future directions
 This is a super minimal project. ~100 lines of code. But it could go into a lot of decent directions without bloating too much. Here are some ideas, in case you'd like to contribute to the project:
 
-* Add some better instructions in this readme or the wiki.
-* Have it start/stop the background helper script on its own.
-* Find a way to get around the sandbox limitation and directly examine the Safari session file.
 * Talk to other instances living on the local network to give an overall measure of the tab counts.
 * Figure out a way to find out the entire current tab situation across one's whole iCloud account.
 * Think up some clever functionality to add to the menu.
 
 If it's at all useful to you, great!
+
 -jeff
--- Tabometer/AppDelegate.swift
+++ Tabometer/AppDelegate.swift
@@ -7,15 +7,14 @@ import Cocoa
 	
 	var statusItem: NSStatusItem?
 	let fileman = FileManager.default
-	var path: String = ""
+	var lastSessionURL: URL?
 	var timer: Timer?
+	let sessionURLKey = "LastSession.plist"
 	
 	override func awakeFromNib() {
 		super.awakeFromNib()
-		
-		let library = fileman.urls(for: .documentDirectory, in: .userDomainMask).first!
-		path = library.appendingPathComponent("LastSession.plist").path
-		
+//		UserDefaults.standard.removeObject(forKey: sessionURLKey)
+
 		statusItem = NSStatusBar.system.statusItem(withLength: 36)
 		
 		let tabInfoView = TabView(frame: NSRect(origin: .zero, size: statusItem!.button!.frame.size))
@@ -26,6 +25,28 @@ import Cocoa
 			statusItem?.button?.addSubview(tabInfoView)
 		}
 		
+		var wentOk = false
+		
+		if let bookmark = UserDefaults.standard.value(forKey: sessionURLKey) as? Data {
+			do {
+				var isStable = false
+				lastSessionURL = try URL(resolvingBookmarkData: bookmark, options: .withSecurityScope, relativeTo: nil, bookmarkDataIsStale: &isStable)
+				
+				startTimer()
+				wentOk = true
+			}
+			catch {
+				print(error.localizedDescription)
+				fatalError()
+			}
+		}
+		
+		if !wentOk {
+			setupSessionURL()
+		}
+	}
+	
+	func startTimer() {
 		timer = Timer.scheduledTimer(withTimeInterval: 30, repeats: true) { t in
 			self.updateCountInfoFromSafari()
 		}
@@ -33,16 +54,54 @@ import Cocoa
 		timer?.fire()
 	}
 	
+	func setupSessionURL() {
+		let openPanel = NSOpenPanel()
+		openPanel.message = "Select the file ~/Library/Safari/LastSession.plist and press the [Select] button"
+		openPanel.prompt = "Select"
+		openPanel.nameFieldLabel = "test 1"
+		openPanel.backgroundColor = .red
+		openPanel.directoryURL = fileman.urls(for: .libraryDirectory, in: .userDomainMask).first!.appendingPathComponent("Safari").appendingPathComponent("LastSession.plist")
+		openPanel.allowsMultipleSelection = false
+		openPanel.canChooseDirectories = false
+		openPanel.canChooseFiles = true
+		
+		openPanel.begin { result in
+			if result == .OK,
+			   let url = openPanel.url,
+			   url.lastPathComponent == self.sessionURLKey {
+				self.lastSessionURL = url
+				self.setup(url: url)
+			}
+			
+			self.startTimer()
+		}
+	}
+	
+	func setup(url: URL) {
+		do {
+			let bookmark = try url.bookmarkData(options: .withSecurityScope, includingResourceValuesForKeys: nil, relativeTo: nil)
+			UserDefaults.standard.setValue(bookmark, forKey: sessionURLKey)
+		}
+		catch {
+			print(error.localizedDescription)
+			fatalError()
+		}
+	}
+	
 	func updateCountInfoFromSafari() {
-		if fileman.fileExists(atPath: path) {
+		if let url = lastSessionURL,
+		   fileman.fileExists(atPath: url.path) {
 			do {
-				let data = try Data(contentsOf: URL(fileURLWithPath: path))
+				let requiresStop = url.startAccessingSecurityScopedResource()
+				
+				let data = try Data(contentsOf: url)
+				
+				if requiresStop { url.stopAccessingSecurityScopedResource() }
 				
 				if PropertyListSerialization.propertyList(data, isValidFor: .binary) {
 					let pls = try PropertyListSerialization.propertyList(from: data, options: .mutableContainersAndLeaves, format: nil)
 					if let d = pls as? [String : Any],
 					   let windows = d["SessionWindows"] as? [[String : Any]] {
-						
 						let tabs = windows.map { w in (w["TabStates"] as! [[String : Any]]).count }.reduce(0, +)
 						
 						if let tabView = self.statusItem?.button?.subviews.first(where: { view in view is TabView }) as? TabView {
@@ -57,6 +116,10 @@ import Cocoa
 				print(error.localizedDescription)
 				fatalError()
 			}
+		}
+		else {
+			UserDefaults.standard.removeObject(forKey: sessionURLKey)
+			setupSessionURL()
 		}
 	}
 	
